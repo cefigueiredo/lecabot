51c1cc3905255977956b833ab00869b02bdf70c6
Carlos Figueiredo <lecaduco@proton.me>
2022-07-30 03:44:44 -0400

    Add tests and ensure unicity for dojo participants

--- lib/lecabot/dojo.ex
+++ lib/lecabot/dojo.ex
@@ -1,24 +1,46 @@
 defmodule Lecabot.Dojo do
+  @moduledoc ~S"""
+  Module that represents a Dojo session.
+  It starts as a GenServer
+
+  ### Examples
+
+      iex> {:ok, dojo_pid} = GenServer.start(Lecabot.Dojo, %Lecabot.Dojo{})
+      {:ok, dojo_pid}
+
+  """
   use GenServer
 
-  alias Lecabot.Dojo, as: Dojo
-
-  defstruct pilot: nil, copilot: nil, audience: []
+  @typedoc """
+  A %Lecabot.Dojo{} struct has the following attributes:
+    * pilot: which stores the username for the current pilot
+    * copilot: that stores the username for the current copilot
+    * audience: that is a MapSet that list of unique usernames that will participate in the session
+  """
+  @type t :: %__MODULE__{
+    pilot: String.t(),
+    copilot: String.t(),
+    audience: MapSet.t(String.t())
+  }
+  defstruct pilot: nil, copilot: nil, audience: MapSet.new
 
   def start_link(state) do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
   @impl GenServer
-  def init(dojo = %Dojo{}) do
+  def init(%__MODULE__{} = dojo) do
     {:ok, dojo}
   end
 
   @impl GenServer
   def handle_cast({:add_participant, participant}, dojo) do
-    audience = [participant | dojo.audience]
+    audience = MapSet.put(dojo.audience, participant)
 
     {:noreply, Map.merge(dojo, %{audience: audience})}
+  end
+  def handle_cast(_request, state) do
+    {:noreply, state}
   end
 
   @impl GenServer
---
+++ test/lecabot/dojo_test.exs
@@ -0,0 +1,61 @@
+defmodule Lecabot.DojoTest do
+  use ExUnit.Case
+  doctest Lecabot.Dojo
+
+  setup do
+    initial_audience = MapSet.new(["username"])
+    {:ok, dojo_pid} = GenServer.start(Lecabot.Dojo, %Lecabot.Dojo{audience: initial_audience})
+
+    [
+      initial_audience: initial_audience,
+      dojo_pid: dojo_pid
+    ]
+  end
+
+  describe "handle_call/2" do
+    test "responds to :dojo msg returning the current dojo state", %{
+      initial_audience: initial_audience,
+      dojo_pid: pid
+    } do
+      %Lecabot.Dojo{audience: audience} = GenServer.call pid, :dojo
+
+      assert initial_audience == audience
+    end
+  end
+
+  describe "handle_cast/2" do
+    test "responds to {:add_participant, \"username2\"} adding the username to audience", %{
+      initial_audience: initial_audience,
+      dojo_pid: pid
+    } do
+      GenServer.cast pid, {:add_participant, "username2"}
+
+      %Lecabot.Dojo{audience: changed_audience} = GenServer.call pid, :dojo
+      expected_audience = MapSet.put(initial_audience, "username2")
+
+      assert expected_audience == changed_audience
+    end
+
+    test "when adding participant avoids duplicates", %{
+      initial_audience: initial_audience,
+      dojo_pid: pid
+    } do
+      :ok = GenServer.cast pid, {:add_participant, "username"}
+
+      %Lecabot.Dojo{audience: retrieved_audience} = GenServer.call pid, :dojo
+
+      assert initial_audience == retrieved_audience
+    end
+
+    test "responds to anything else doing nothing", %{
+      initial_audience: initial_audience,
+      dojo_pid: pid
+    } do
+      GenServer.cast pid, :whatever
+
+      %Lecabot.Dojo{audience: current_state} = GenServer.call pid, :dojo
+
+      assert initial_audience == current_state
+    end
+  end
+end
