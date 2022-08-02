8d8522d7a9edaa1defdd493ae147bc715f6b6aa2
Carlos Figueiredo <lecaduco@proton.me>
2022-08-02 00:40:36 -0400

    Refactor contexts passed to tests

--- test/lecabot/dojo_test.exs
+++ test/lecabot/dojo_test.exs
@@ -13,47 +13,41 @@ defmodule Lecabot.DojoTest do
   end
 
   describe "handle_call/2 :dojo" do
-    test "responds returning the current dojo state", %{
-      initial_audience: initial_audience,
-      dojo_pid: pid
-    } do
-      %Lecabot.Dojo{audience: audience} = GenServer.call(pid, :dojo)
+    test "responds returning the current dojo state", context do
+      %Lecabot.Dojo{audience: audience} = GenServer.call(context.dojo_pid, :dojo)
 
-      assert initial_audience == audience
+      assert context.initial_audience == audience
     end
   end
 
-  describe "handle_cast/2 {:add_participant, _name}" do
-    test "responds adding the username to audience", %{
-      initial_audience: initial_audience,
-      dojo_pid: pid
-    } do
-      GenServer.cast(pid, {:add_participant, "username2"})
+  describe "handle_cast/2 {:add_participant, _username}" do
+    test "responds adding the username to audience", context do
+      GenServer.cast(context.dojo_pid, {:add_participant, "username2"})
 
-      %Lecabot.Dojo{audience: changed_audience} = GenServer.call(pid, :dojo)
-      expected_audience = MapSet.put(initial_audience, "username2")
+      %Lecabot.Dojo{audience: audience} = GenServer.call(context.dojo_pid, :dojo)
+      expected_audience = MapSet.put(context.initial_audience, "username2")
 
-      assert expected_audience == changed_audience
+      assert expected_audience == audience
     end
 
-    test "avoids duplicates", %{
-      initial_audience: initial_audience,
-      dojo_pid: pid
-    } do
-      :ok = GenServer.cast(pid, {:add_participant, "username"})
+    test "avoids duplicates", context do
+      :ok = GenServer.cast(context.dojo_pid, {:add_participant, "username"})
 
-      %Lecabot.Dojo{audience: retrieved_audience} = GenServer.call(pid, :dojo)
+      %Lecabot.Dojo{audience: audience} = GenServer.call(context.dojo_pid, :dojo)
 
-      assert initial_audience == retrieved_audience
+      assert context.initial_audience == audience
     end
   end
 
   describe "handle_cast/2 when msg is unknown" do
-    test "responds doing nothing", %{
-      initial_audience: initial_audience,
-      dojo_pid: pid
-    } do
-      GenServer.cast(pid, :whatever)
+    test "responds doing nothing", context do
+      GenServer.cast(context.dojo_pid, :whatever)
+
+      %Lecabot.Dojo{audience: current_state} = GenServer.call(context.dojo_pid, :dojo)
+
+      assert context.initial_audience == current_state
+    end
+  end
 
       %Lecabot.Dojo{audience: current_state} = GenServer.call(pid, :dojo)
 
