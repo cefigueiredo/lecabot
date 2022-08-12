2f023d5d4ddd3ee6e248721d9aba91a8f8e5ec2e
Carlos Figueiredo <lecaduco@proton.me>
2022-08-11 22:53:23 -0400

    Accept access from main host machine

--- config/dev.exs
+++ config/dev.exs
@@ -33,7 +33,7 @@ config :lecabot, Lecabot.Repo,
 config :lecabot_web, LecabotWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
